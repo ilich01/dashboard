371b0a4f94489493cb6239e1d1ddacbab93fa321
ilich01 <sidea.ilyaderkach@gmail.com>
2024-05-31 18:29:18 +0700

    feat: ecommerce page

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,7 +1,15 @@
 import React from 'react'
 
-const Button = () => {
-  return <div>Button</div>
+const Button = ({ backgroundColor, color, size, text, borderRadius }) => {
+  return (
+    <button
+      type="button"
+      style={{ backgroundColor, color, borderRadius }}
+      className={`text-${size} p-3 hover:drop-dash-shadow-xl`}
+    >
+      {text}
+    </button>
+  )
 }
 
 export default Button
