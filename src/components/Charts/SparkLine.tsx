6c58e999a8ac0b10c60dd3c5acb5fc48ed818d0d
ilich01 <sidea.ilyaderkach@gmail.com>
2024-06-04 13:37:22 +0700

    feat: mainpag

--- src/components/Charts/SparkLine.tsx
+++ src/components/Charts/SparkLine.tsx
@@ -1,7 +1,18 @@
 import React from 'react'
+import { SparklineComponent, Inject, SparklineTooltip } from '@syncfusion/ej2-react-charts'
 
-const SparkLine = () => {
-  return <div>SparkLine</div>
+type SparkLineProps = {
+  currentColor: string
+  id: string
+  color: string
+  type: string
+  height: string
+  width: string
+  data: object
+}
+
+const SparkLine: React.FC<SparkLineProps> = ({ currentColor, id, color, type, height, width, data }) => {
+  return <SparklineComponent id={id} height={height} width={width} lineWidth={1} valueType="Numeric"></SparklineComponent>
 }
 
 export default SparkLine
