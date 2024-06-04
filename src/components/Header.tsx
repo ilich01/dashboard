7c10158fe9f75d18802db936236bac1e61a4d712
ilich01 <sidea.ilyaderkach@gmail.com>
2024-06-04 18:23:24 +0700

    feat: add orders

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,16 @@
 import React from 'react'
+type HeaderProps = {
+  title: string
+  category: string
+}
 
-function Header() {
-  return <div>Header</div>
+const Header: React.FC<HeaderProps> = ({ title, category }) => {
+  return (
+    <div className="m-10">
+      <p className="text-gray-200">{category}</p>
+      <p className="text-3xl font-extrabold tracking-tight text-slate-900 ">{title}</p>
+    </div>
+  )
 }
 
 export default Header
