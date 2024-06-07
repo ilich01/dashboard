2873361883f2f4220bcfafc87bbd7454e8bf2ebd
ilich01 <sidea.ilyaderkach@gmail.com>
2024-06-07 17:44:17 +0700

    fix

--- src/components/Charts/LineChart.tsx
+++ src/components/Charts/LineChart.tsx
@@ -1,7 +1,41 @@
 import React from 'react'
+import {
+  ChartComponent,
+  SeriesCollectionDirective,
+  SeriesDirective,
+  Inject,
+  DateTime,
+  Legend,
+  Tooltip,
+  LineSeries,
+  AxisModel,
+} from '@syncfusion/ej2-react-charts'
+import { lineCustomSeries, LinePrimaryXAxis, LinePrimaryYAxis } from 'data/dummy'
+import { useStateContext } from 'context/ContextProvider'
 
 const LineChart = () => {
-  return <div>LineChart</div>
+  const { currentMode } = useStateContext()
+  return (
+    <div>
+      <ChartComponent
+        id="line-chart"
+        height="420px"
+        primaryXAxis={LinePrimaryXAxis as AxisModel}
+        primaryYAxis={LinePrimaryYAxis as AxisModel}
+        chartArea={{ border: { width: 0 } }}
+        tooltip={{ enable: true }}
+        background={currentMode === 'Dark' ? '#33373E' : '#fff'}
+        legendSettings={{ background: 'white' }}
+      >
+        <Inject services={[LineSeries, Legend, DateTime, Tooltip]} />
+        <SeriesCollectionDirective>
+          {lineCustomSeries.map((item, index) => (
+            <SeriesDirective key={index} {...item} />
+          ))}
+        </SeriesCollectionDirective>
+      </ChartComponent>
+    </div>
+  )
 }
 
 export default LineChart
--- src/pages/Charts/Line.tsx
+++ src/pages/Charts/Line.tsx
@@ -1,7 +1,14 @@
 import React from 'react'
-import { ChartsHeader, LineChart } from '../../components'
+import { ChartsHeader, Header, LineChart } from '../../components'
 const Line = () => {
-  return <div>Line</div>
+  return (
+    <div className="m-4 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+      <Header title="Inflation rate" category="Chart" />
+      <div className="w-full">
+        <LineChart />
+      </div>
+    </div>
+  )
 }
 
 export default Line
